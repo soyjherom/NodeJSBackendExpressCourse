dac8fec1c055f4174e1603b0bc5d50828876f0a7
soyjherom <jherom@tecnica.co.cr>
2023-02-24 22:57:08 -0600

    [ADD] CORS whitelist

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
+const boom = require('@hapi/boom');
+const cors = require('cors');
 const express = require('express');
-const { boomErrorHandler, errorHandler, logErrors } = require('./middleWares/error.handler');
 const routersApi = require('./routes')
+
+const { boomErrorHandler, errorHandler, logErrors } = require('./middleWares/error.handler');
+
 const app = express();
 const port = 3021;
 
 app.use(express.json())
+
+const whitelist = ['http://localhost:8083', 'https://myapp.co']
+const options = {
+  origin: (origin,callback)=>{
+    if(whitelist.includes(origin)) callback(null, true)
+    else callback(boom.notAcceptable('Unacceptable Origin'));
+  }
+}
+app.use(cors(options));
 
 app.get('/',(req, res)=>{
   res.writeHead(200,{'Content-Type':'html'})
